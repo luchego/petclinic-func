9e4d6da5167ed90866d1acca88adb3f753d09c4d
Luis Castillo Elera <luis_castle_2011@hotmail.com>
2018-04-26 00:40:48 -0500

    Commit Fork para prueba de Jenkins

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -14,7 +14,7 @@ public class NewTest {
 	    private WebDriver driver;		
 		@Test				
 		public void testEasy() {	
-			driver.get("http://localhost:8090/petclinic/");  
+			driver.get("http://localhost:8085/petclinic/");  
 			String title = driver.getTitle();			
 			System.out.println(">>>>>>"+title);
 			Assert.assertTrue(title.contains("a Spring Framework")); 		
@@ -26,7 +26,7 @@ public class NewTest {
 			//System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
 			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mitesh\\Downloads\\chromedriver_win32\\chromedriver.exe");
 			//driver = new ChromeDriver();  
-			File file = new File("C:\\Users\\Mitesh\\Downloads\\geckodriver-v0.13.0-win64\\geckodriver.exe");
+			File file = new File("D:\\Desarrollo de Software\\DevOps\\Repositorios\\geckodriver-v0.13.0-win64\\geckodriver.exe");
 			System.setProperty("webdriver.gecko.driver", file.getAbsolutePath());			
 			
 			
